6e22b6d31ca0e70f0acfaee249f35a83c15bf9cd
John Shahbazian <jshahbazi@gmail.com>
2024-12-05 14:20:51 -0500

    Day4 part 2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -77,15 +77,39 @@ fn main() -> io::Result<()> {
     let file = File::open("./input.txt")?;
     let reader = BufReader::new(file);
 
-    let xmas_pattern = Regex::new(r"(?P<XMAS>XMAS)").unwrap();
+    // let xmas_pattern = Regex::new(r"(?P<XMAS>XMAS)").unwrap();
+
+    let xmas_pattern = vec![
+        vec![Some('M'), None, Some('S')],
+        vec![None, Some('A'), None],
+        vec![Some('M'), None, Some('S')],
+    ];
+
+    let reverse_xmas_pattern = vec![
+        vec![Some('S'), None, Some('M')],
+        vec![None, Some('A'), None],
+        vec![Some('S'), None, Some('M')],
+    ];    
+
+    let top_down_xmas_pattern = vec![
+        vec![Some('M'), None, Some('M')],
+        vec![None, Some('A'), None],
+        vec![Some('S'), None, Some('S')],
+    ];
+
+    let bottom_up_xmas_pattern = vec![
+        vec![Some('S'), None, Some('S')],
+        vec![None, Some('A'), None],
+        vec![Some('M'), None, Some('M')],
+    ];        
 
     let mut sum: i32 = 0;
 
     let mut grid: Vec<Vec<char>> = Vec::new();
-    let mut vec_of_strings: Vec<String> = Vec::new();
+    // let mut vec_of_strings: Vec<String> = Vec::new();
     for line in reader.lines() {
-        vec_of_strings.push(line.as_ref().unwrap().clone());
-        vec_of_strings.push(line.as_ref().unwrap().chars().rev().collect());
+        // vec_of_strings.push(line.as_ref().unwrap().clone());
+        // vec_of_strings.push(line.as_ref().unwrap().chars().rev().collect());
 
         let mut line_vector = Vec::new();
         for character in line?.chars() {
@@ -95,40 +119,139 @@ fn main() -> io::Result<()> {
         grid.push(line_vector);
     }
 
-    for col in 0..grid[0].len() {
-        let mut word = String::new();
-        for row in &grid {
-            word.push(row[col]);
-        }
-        vec_of_strings.push(word.clone());
-        vec_of_strings.push(word.chars().rev().collect());
-    }
-    
-    let top_left_diagonals = get_top_left_diagonals(&grid);
-    let top_right_diagonals = get_top_right_diagonals(&grid);
+    let grid_height = grid.len();
+    let grid_width = grid[0].len();
+    let pattern_height = xmas_pattern.len();
+    let pattern_width = xmas_pattern[0].len();
 
-    for diagonal in top_left_diagonals {
-        let word: String = diagonal.iter().collect();
-        vec_of_strings.push(word.clone());
-        let reversed_word: String = word.chars().rev().collect();
-        vec_of_strings.push(reversed_word);
-    }
 
-    for diagonal in top_right_diagonals {
-        let word: String = diagonal.iter().collect();
-        vec_of_strings.push(word.clone());
-        let reversed_word: String = word.chars().rev().collect();
-        vec_of_strings.push(reversed_word);
-    }
+    for i in 0..=grid_height - pattern_height {
+        for j in 0..=grid_width - pattern_width {
+            let mut match_found = true;
 
-    for line in vec_of_strings {
-        let text = line;
-        for caps in xmas_pattern.captures_iter(&text) {
-            if let Some(_xmas_match) = caps.name("XMAS") {
-                sum += 1;
+            for (p_i, pattern_row) in xmas_pattern.iter().enumerate() {
+                for (p_j, pattern_cell) in pattern_row.iter().enumerate() {
+                    match pattern_cell {
+                        Some(c) => {
+                            if grid[i + p_i][j + p_j] != *c {
+                                match_found = false;
+                                break;
+                            }
+                        }
+                        None => continue, // Wildcard matches any character
+                    }
+                }
+                if !match_found {
+                    break;
+                }
             }
+            if match_found {
+                sum += 1;
+                // println!("Pattern found at position ({}, {})", i, j);
+            }
+
+            match_found = true;
+            for (p_i, pattern_row) in reverse_xmas_pattern.iter().enumerate() {
+                for (p_j, pattern_cell) in pattern_row.iter().enumerate() {
+                    match pattern_cell {
+                        Some(c) => {
+                            if grid[i + p_i][j + p_j] != *c {
+                                match_found = false;
+                                break;
+                            }
+                        }
+                        None => continue, // Wildcard matches any character
+                    }
+                }
+                if !match_found {
+                    break;
+                }
+            }
+            if match_found {
+                sum += 1;
+                // println!("Reverse pattern found at position ({}, {})", i, j);
+            }   
+
+            match_found = true;
+            for (p_i, pattern_row) in top_down_xmas_pattern.iter().enumerate() {
+                for (p_j, pattern_cell) in pattern_row.iter().enumerate() {
+                    match pattern_cell {
+                        Some(c) => {
+                            if grid[i + p_i][j + p_j] != *c {
+                                match_found = false;
+                                break;
+                            }
+                        }
+                        None => continue, // Wildcard matches any character
+                    }
+                }
+                if !match_found {
+                    break;
+                }
+            }
+            if match_found {
+                sum += 1;
+                // println!("Reverse pattern found at position ({}, {})", i, j);
+            }    
+
+            match_found = true;
+            for (p_i, pattern_row) in bottom_up_xmas_pattern.iter().enumerate() {
+                for (p_j, pattern_cell) in pattern_row.iter().enumerate() {
+                    match pattern_cell {
+                        Some(c) => {
+                            if grid[i + p_i][j + p_j] != *c {
+                                match_found = false;
+                                break;
+                            }
+                        }
+                        None => continue, // Wildcard matches any character
+                    }
+                }
+                if !match_found {
+                    break;
+                }
+            }
+            if match_found {
+                sum += 1;
+                // println!("Reverse pattern found at position ({}, {})", i, j);
+            }                                 
         }
-    }
+    }    
+
+    // for col in 0..grid[0].len() {
+    //     let mut word = String::new();
+    //     for row in &grid {
+    //         word.push(row[col]);
+    //     }
+    //     vec_of_strings.push(word.clone());
+    //     vec_of_strings.push(word.chars().rev().collect());
+    // }
+    
+    // let top_left_diagonals = get_top_left_diagonals(&grid);
+    // let top_right_diagonals = get_top_right_diagonals(&grid);
+
+    // for diagonal in top_left_diagonals {
+    //     let word: String = diagonal.iter().collect();
+    //     vec_of_strings.push(word.clone());
+    //     let reversed_word: String = word.chars().rev().collect();
+    //     vec_of_strings.push(reversed_word);
+    // }
+
+    // for diagonal in top_right_diagonals {
+    //     let word: String = diagonal.iter().collect();
+    //     vec_of_strings.push(word.clone());
+    //     let reversed_word: String = word.chars().rev().collect();
+    //     vec_of_strings.push(reversed_word);
+    // }
+
+    // for line in vec_of_strings {
+    //     let text = line;
+    //     for caps in xmas_pattern.captures_iter(&text) {
+    //         if let Some(_xmas_match) = caps.name("XMAS") {
+    //             sum += 1;
+    //         }
+    //     }
+    // }
 
     println!("Sum: {}", sum);
 
